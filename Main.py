d862f531ddf142aa41858c0258eecd6ffad845c2
Lusca05 <40044388+Lusca05@users.noreply.github.com>
2019-10-28 20:26:36 -0300

    navegando em pasta

--- Main.py
+++ Main.py
@@ -1,89 +1,82 @@
+import os
+
 from ListaEncadeada import *
 from Aresta import *
 from Vertice import *
 class Main:
+    def __init__(self,pasta):
+        self.lista = []
+        self.pasta = pasta
+        self.vertice = 0
+        self.aresta = 0
+        self.cont = 0
+
     def index(self,vetor, lista):
         for cont in range(len(lista)):
             if vetor == lista[cont].info:
 
                 return cont
-    def criarLista(self):
-        loop = True
-        cont = 1
-        lista = []
-        while loop:
-            try:
-                print("C:/Users/augusto.coelho/PycharmProjects/Dijjkstra/Base de Enron/brawner-s/all_documents/" + str(cont))
-                f = open("C:/Users/augusto.coelho/PycharmProjects/Dijjkstra/Base de Enron/brawner-s/all_documents/" + str(cont))
-                print("2")
-                lines = f.readlines()
-                linha1 = lines[2][6:].replace('\n', '')
-                linha2 = lines[3][4:].replace('\n', '')
-                v1 = Vertice(linha1)
-                v2 = Vertice(linha2)
-                if v1 in lista and v2 in lista:
-                    indexV1 = lista.index(v1)
-                    indexV2 = lista.index(v2)
-                    lista[indexV1].listaEncadeada.atualizar(indexV2)
-                else:
 
-                    if v1 not in lista:
-                        lista.append(v1)
-                    if v2 not in lista:
-                        lista.append(v2)
-                    indexV1 = lista.index(v1)
-                    indexV2 = lista.index(v2)
-                    lista[indexV1].listaEncadeada.insere(indexV2,1)
-                f.close()
-            except:
-                print("MAS UE")
-                loop = False
+    def listar_pasta(self, dir):
+        subs = os.listdir(dir)
+        for sub in subs:
+            path = dir + '\\' + sub
+            if os.path.isdir(path):
+                self.listar_pasta(path)
+            elif os.path.isfile(path):
+                self.criarLista(path)
 
-            return lista
+    def criarLista(self, path):
 
+        f = open(path,'r')
+
+        lines = f.readlines()
+        linha1 = lines[2][6:].replace('\n', '')
+        linha2 = lines[3][4:].replace('\n', '')
+        v1 = Vertice(linha1)
+        v2 = Vertice(linha2)
+        for i in range(len(self.lista)):
+
+        if v1 in self.lista and v2 in self.lista:
+            indexV1 = self.lista.index(v1)
+            indexV2 = self.lista.index(v2)
+            self.lista[indexV1].listaEncadeada.atualizar(indexV2)
+        elif v1 in self.lista and v2 not in self.lista:
+            self.lista.append(v2)
+            indexV1 = self.lista.index(v1)
+            indexV2 = self.lista.index(v2)
+            self.cont += 1
+            self.lista[indexV1].listaEncadeada.insere(indexV2,1)
+        elif v1 not in self.lista and v2 in self.lista:
+            self.lista.append(v1)
+            indexV1 = self.lista.index(v1)
+            indexV2 = self.lista.index(v2)
+            self.cont += 1
+            self.lista[indexV1].listaEncadeada.insere(indexV2, 1)
+        else:
+
+            self.lista.append(v1)
+            self.lista.append(v2)
+            self.cont+=2
+            indexV1 = self.lista.index(v1)
+            indexV2 = self.lista.index(v2)
+            self.lista[indexV1].listaEncadeada.insere(indexV2,1)
 
 
 
 
     def inicio(self):
 
-        '''v1 = Vertice('A')
-        v2 = Vertice('G')
-        v3 = Vertice('H')
-        v4 = Vertice('I')
-        v5 = Vertice('B')
-        v6 = Vertice('E')
-        v7 = Vertice('D')
-        v8 = Vertice('K')
-
-        lista = [v1,
-                 v2,
-                 v3,
-                 v4,
-                 v5,
-                 v6,
-                 v7,
-                 v8]
-
-        lista[0].listaEncadeada.insere(self.index('G',lista), 8)
-
-        lista[0].listaEncadeada.insere(self.index('H',lista), 5)
-        lista[0].listaEncadeada.insere(self.index('I',lista), 1)
-        lista[1].listaEncadeada.insere(self.index('B',lista), 2)
-        lista[1].listaEncadeada.insere(self.index('E',lista), 4)
-        lista[2].listaEncadeada.insere(self.index('D',lista), 3)
-        lista[2].listaEncadeada.insere(self.index('K',lista), 6)
-        '''
         soma = 0
-        lista = self.criarLista()
-        for cont in lista:
+        self.listar_pasta(self.pasta)
+        for cont in self.lista:
             print(soma,'[', cont.info, ']', end='->')
             cont.listaEncadeada.printarLista()
             print()
             soma+=1
 
 def main():
-    comeco = Main()
+    comeco = Main('Base de Enron')
     comeco.inicio()
 
 
